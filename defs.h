94aaaebac4fb30376140c1065e6e2f4eb62a2fa0
cosunae <carlos.osuna@env.ethz.ch>
2017-04-09 23:21:12 +0200

    adding code for avgs

--- defs.h
+++ defs.h
@@ -3,5 +3,5 @@
 #define BLOCKSIZEX 32
 #define BLOCKSIZEY 8
 
-enum stencils { copy_st=0, copyi1_st, sumi1_st, sumj1_st, sumk1_st, lap_st, num_bench_st };
+enum stencils { copy_st=0, copyi1_st, sumi1_st, sumj1_st, sumk1_st, avgi_st, avgj_st, avgk_st, lap_st, num_bench_st };
 
